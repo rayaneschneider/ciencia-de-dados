c43426860196e8b3ced52750ac36eb40a870c447
rayaneschneider <105427657+rayaneschneider@users.noreply.github.com>
2024-10-04 08:30:22 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,5 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-console.log)(url);
+console.log(url);
 async function visualizarInformacoesGlobais() {
     const res = await fetch (url)
     const dados = res.json ()
