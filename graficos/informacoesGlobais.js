b2c7edb10e40c9ec9e608b07bcd7591c05d59447
rayaneschneider <105427657+rayaneschneider@users.noreply.github.com>
2024-10-25 08:41:36 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
     const pessoasNoMundo = (dados.total_pessoas_mundo)  / 1e9
     const horas = parseInt (dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas * 100))
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
