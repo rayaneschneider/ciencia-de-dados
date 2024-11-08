aaba8cb54f0ea4a6d3eeb30ed66cb2ddf1097171
rayaneschneider <105427657+rayaneschneider@users.noreply.github.com>
2024-11-08 08:42:46 -0300

    ok

--- graficos/common.js
+++ graficos/common.js
@@ -1,10 +1,10 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(ducument.body)
+    const bodyStyles = getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
 }
 
 const tickConfig = { 
-    family: getCSS(' ---font'),
+    family: getCSS('--font'),
     size:16,
     color: getCSS('--primary-color')
 }
