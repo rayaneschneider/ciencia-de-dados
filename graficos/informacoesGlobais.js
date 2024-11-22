f2985ae6e92a9fe655d7a64372f553baa516576b
rayaneschneider <105427657+rayaneschneider@users.noreply.github.com>
2024-11-22 08:41:20 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,17 +1,16 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
+const url = 'https://raw.githubusercontent.com/rayaneschneider/ciencia-de-dados/refs/heads/main/base-de-dados/esportes-dados-globais.json'
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasPraticantes = (dados.total_pessoas_que_praticam_esportes_regularmente) / 1e9
     const pessoasNoMundo = (dados.total_pessoas_mundo)  / 1e9
-    const horas = parseInt (dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+    const horas = parseInt (dados.tempo_medio_semana_praticando_esportes)
+    const minutos = Math.round((dados.tempo_medio_semana_praticando_esportes - horas) * 100)
+    const porcentagemPraticantes = ((pessoasPraticantes / pessoasNoMundo) * 100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.
-    <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> de pessoas e que aproximadamente <span>${pessoasPraticantes} bilhões</span> praticam esportes e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos praticando.
+    <br> Isso significa que aproximadamente ${porcentagemPraticantes}% de pessoas praticam algum esporte.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
   }
